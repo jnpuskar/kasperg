3497be4e2387f300faefdde6877c24a7b0ba4c60
jnpuskar <jan.puskar@tokensec.com>
2016-03-01 18:18:50 +0100

    work

---
+++ solver/Pin.h
@@ -0,0 +1,16 @@
+#include <Windows.h>
+
+// Direction definition
+//       2       1
+//        \     /
+//         \   /
+//   3 ---   6  --- 0
+//         /   \ 
+//        /	    \
+//       4       5
+
+class CPin
+{
+private:
+	unsigned char _name;
+};
