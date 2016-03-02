a977e6f0ae3f72e2fcdf6542fe8c6699123fce66
jnpuskar <jan.puskar@gmail.com>
2016-03-02 01:15:53 +0100

    constants

--- solver/Pin.h
+++ solver/Pin.h
@@ -9,8 +9,43 @@
 //        /	    \
 //       4       5
 
+
+const char NO_PIN	= -1;
+const char A_PIN = 1;
+const char B_PIN = 1;
+const char C_PIN = 1;
+const char D_PIN = 1;
+const char E_PIN = 1;
+const char F_PIN = 1;
+const char G_PIN = 1;
+const char H_PIN = 1;
+const char I_PIN = 1;
+const char J_PIN = 1;
+const char K_PIN = 1;
+const char L_PIN = 1;
+const char M_PIN = 1;
+const char N_PIN = 1;
+const char O_PIN = 1;
+const char P_PIN = 1;
+const char Q_PIN = 1;
+const char R_PIN = 1;
+const char S_PIN = 1;
+const char T_PIN = 1;
+const char U_PIN = 1;
+const char V_PIN = 1;
+const char W_PIN = 1;
+const char X_PIN = 1;
+
+const char A_PIN = 1;
+const char A_PIN = 1;
+const char A_PIN = 1;
+const char A_PIN = 1;
+const char A_PIN = 1;
+
+
 class CPin
 {
 private:
 	unsigned char _name;
+	std::vector<
 };
